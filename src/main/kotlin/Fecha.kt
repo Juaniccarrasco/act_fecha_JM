3aa1df48e37c1d89ab307d0b68b8473c372e77c9
Juan C <juaniccarrasco@gmail.com>
2024-03-10 09:29:19 +0100

    Como estaba antes de git reset. Parto de aquí para hacer git reverse y juntar las lineas luego.

--- src/main/kotlin/Fecha.kt
+++ src/main/kotlin/Fecha.kt
@@ -1,23 +1,35 @@
-class Fecha(private val dias: Int, private val mes: Int, private val a: Int) {
+/**
+ * Fecha sirve para decirte si la fecha de cualquier día es valida
+ *
+ * @property diasIntroducido
+ * @property mesIntroducido
+ * @property annoIntroducido
+ * @constructor Create empty Fecha
+ */
+class Fecha(private val diasIntroducido: Int, private val mesIntroducido: Int, private val annoIntroducido: Int) {
 
+    /**
+     * Valida te dice si el dia y el mes introducidos se encuentran en un rango existente del año
+     *
+     * @return devuelve un boleano diciendo si esa fecha existe o no
+     */
+    fun fechaEsValida(): Boolean {
 
-    fun valida(): Boolean {
-
-        if (dias < 1 || dias > 31) return false
-        if (mes < 1 || mes > 12) return false
+        if (diasIntroducido < 1 || diasIntroducido > 31) return false
+        if (mesIntroducido < 1 || mesIntroducido > 12) return false
 
         // determinamos la cantidad de días del mes:
-        return diasdelMes()
+        return devolverDiasDelMes()
     } // … más métodos
 
-    private fun diasdelMes(): Boolean {
+    private fun devolverDiasDelMes(): Boolean {
         var diasMes = 0
-        when (mes) {
+        when (mesIntroducido) {
             1, 3, 5, 7, 8, 10, 12 -> diasMes = 31
             4, 6, 9, 11 -> diasMes = 30
-            2 -> diasMes = if (a % 400 == 0 || a % 4 == 0 && a % 100 != 0) 29 else 28
+            2 -> diasMes = if (annoIntroducido % 400 == 0 || annoIntroducido % 4 == 0 && annoIntroducido % 100 != 0) 29 else 28
         }
         return if (
-            dias > diasMes) false else true
+            diasIntroducido > diasMes) false else true
     }
 }
--- src/test/kotlin/FechaTest.kt
+++ src/test/kotlin/FechaTest.kt
@@ -17,36 +17,36 @@ class FechaTest {
     @Test
     fun valida() {
         val fecha = Fecha(15, 13, 2023)
-        assertTrue (fecha.valida())
+        assertTrue (fecha.fechaEsValida())
     }
 
 
     @Test
     fun fechaInvalidaDia() {
         val fecha = Fecha(32, 5, 2022)
-        assertFalse (fecha.valida())
+        assertFalse (fecha.fechaEsValida())
     }
 
     @Test
     fun fechaInvalidaMes() {
         val fecha = Fecha(12, 13, 2021)
         // Assert.assertFalse(fecha.valida())
-        assertFalse (fecha.valida())
+        assertFalse (fecha.fechaEsValida())
     }
 
     @Test
     fun fechaInvalidaFebreroBisiesto() {
         val fecha = Fecha(29, 2, 2021)
-        assertFalse (fecha.valida())
+        assertFalse (fecha.fechaEsValida())
         println("Fecha invalida")
-        println(fecha.valida())
+        println(fecha.fechaEsValida())
     }
 
     @Test
     fun fechaValidaFebreroBisiesto() {
         val fecha = Fecha(29, 2, 2024)
-        assertTrue (fecha.valida())
+        assertTrue (fecha.fechaEsValida())
         println("Fecha valida Bisiesto")
-        println(fecha.valida())
+        println(fecha.fechaEsValida())
     }
 }
